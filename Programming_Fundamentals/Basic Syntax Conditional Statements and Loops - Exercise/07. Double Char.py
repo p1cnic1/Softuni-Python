ee786b8fe7268f71c4d9fe14a185e53eb18eecb7
p1cnic1 <123311721+p1cnic1@users.noreply.github.com>
2023-04-18 07:02:44 -0700

    sds

--- Programming_Fundamentals/Basic Syntax Conditional Statements and Loops - Exercise/07. Double Char.py
+++
@@ -1,9 +0,0 @@
-
-string = input()
-while string != "End":
-    if string != 'SoftUni':
-        for char in string:
-            print(char, end='')
-            print(char, end='')
-        print()
-    string = input()
